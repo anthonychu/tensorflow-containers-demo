5d83717e2841b00ff4853584c9a3422ddef97ad8
Anthony Chu <antchu@microsoft.com>
2018-05-06 22:25:18 -0700

    Initial commit

---
+++ src/Classifier.ImageDownloader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Classifier.ImageDownloader
+{
+    class Program
+    {
+        private static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            var folder = args[0];
+            var images = JsonConvert.DeserializeObject<ImageMetadata[]>(File.ReadAllText($"{folder}/images.json"));
+
+            foreach (var image in images)
+            {
+                using (var fs = new FileStream($"{folder}/{image.ImageId}.{image.EncodingFormat}", FileMode.Create))
+                {
+                    using (var stream = client.GetStreamAsync(image.ContentUrl).Result)
+                    {
+                        stream.CopyTo(fs);
+                        fs.Close();
+                    }
+                }
+            }
+        }
+    }
+
+    public class ImageMetadata
+    {
+        public string ImageId { get; set; }
+        public string EncodingFormat { get; set; }
+        public string ContentUrl { get; set; }
+    }
+}
---
+++ src/Classifier.Worker/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using TensorFlow;
+
+namespace Classifier.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var images = JsonConvert.DeserializeObject<ImageMetadata[]>(File.ReadAllText("assets/images/images.json"));
+            var rand = new Random();
+            var graph = new TFGraph();
+            var model = File.ReadAllBytes("assets/model.pb");
+            var labels = File.ReadAllLines("assets/labels.txt");
+            var hostname = System.Net.Dns.GetHostName();
+            var jsonSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            var httpClient = new HttpClient();
+
+            graph.Import(model);
+
+            var sw = new Stopwatch();
+            while(true) {
+                using (var session = new TFSession(graph))
+                {
+                    var image = images[rand.Next(images.Length)];
+                    sw.Reset();
+                    sw.Start();
+                    var tensor = ImageUtil.CreateTensorFromImageFile($"assets/images/{image.ImageId}.{image.EncodingFormat}");
+                    var runner = session.GetRunner();
+                    runner.AddInput(graph["Placeholder"][0], tensor).Fetch(graph["loss"][0]);
+                    var output = runner.Run();
+                    var result = output[0];
+                    var apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL") ?? "http://localhost:5000";
+                    
+                    var probabilities = ((float[][])result.GetValue(jagged: true))[0];
+                    var highestProbability = probabilities
+                        .Select((p, i) => (Probability: p, Index: i))
+                        .OrderByDescending(p => p.Probability)
+                        .First();
+                    var bestResult = (Label: labels[highestProbability.Index], Probability: highestProbability.Probability);
+                    Thread.Sleep(Convert.ToInt32(sw.ElapsedMilliseconds)+1000);
+                    sw.Stop();
+
+                    var classificationResult = new ClassificationResult
+                    {
+                        Image = image,
+                        Label = bestResult.Label,
+                        Probability = bestResult.Probability,
+                        WorkerId = hostname,
+                        TimeTaken = sw.ElapsedMilliseconds
+                    };
+
+                    Console.WriteLine(JsonConvert.SerializeObject(classificationResult, jsonSettings));
+                    var content = new StringContent(
+                        JsonConvert.SerializeObject(classificationResult, jsonSettings),
+                        Encoding.UTF8,
+                        "application/json"
+                    );
+                    try
+                    {
+                        httpClient.PostAsync($"{apiBaseUrl}/api/imageprocessed", content).Wait();
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine(e.ToString());
+                    }
+
+                    tensor.Dispose();
+                    foreach(var o in output)
+                    {
+                        o.Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
